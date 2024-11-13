8b98dce66374bf28548f2f19e4a6da0645571ba2
Akshat Gusain <106693158+akshat-gusain@users.noreply.github.com>
2024-11-13 14:21:43 +0530

    Add files via upload

---
+++ EmployeeAttendance.Core/Entities/EmpAttendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance.Core.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; } 
+        public string EmployeeCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string PAN { get; set; }
+        public string Aadhaar { get; set; }
+    }
+
+    public class EmployeeAttendanceRecord
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public DateTime AttendanceDate { get; set; }
+        public string Status { get; set; } 
+    }
+}
+
---
+++ EmployeeAttendance.Core/Repositories/MyEmpRepo.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeAttendance.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace EmployeeAttendance.Core.Repositories
+{
+    public interface MyEmpRepo
+    {
+        Task<Employee> GetEmployeeById(int employeeId);
+        Task<IEnumerable<Employee>> GetAllEmployees();
+        Task AddEmployee(Employee employee);
+        Task UpdateEmployee(Employee employee);
+        Task DeleteEmployee(int employeeId);
+    }
+
+    public interface IEmployeeAttendanceRepository
+    {
+        Task<IEnumerable<EmployeeAttendanceRecord>> GetAttendanceByEmployeeId(int employeeId);
+        Task AddAttendance(EmployeeAttendanceRecord attendance);
+        Task<EmployeeAttendanceRecord> GetAttendanceByEmployeeAndDate(int employeeId, DateTime attendanceDate); 
+        Task<IEnumerable<EmployeeAttendanceRecord>> GetAllAttendanceRecords();
+        Task UpdateAttendance(EmployeeAttendanceRecord attendanceRecord); 
+        Task DeleteAttendance(int employeeId, DateTime attendanceDate);
+        
+    }
+}
